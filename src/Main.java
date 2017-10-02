ce15c1a4f9439845c1bfdea4568a3dbe5340cec9
suhan0902 <ao09020227@gmail.com>
2017-10-02 14:59:52 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("學號:106022053");
+	System.out.println("系級:行動商務與多媒體應用組");
+	System.out.println("姓名:蘇靖涵");
+        System.out.println("時間:2017/9/19");
+        System.out.println("地點:I319");
+
+    }
+}
